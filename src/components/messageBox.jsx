08cd2ef07fd700fc08c5193e99a85c38de249c6c
Eirik Årdal <eaardal@outlook.com>
2016-02-23 19:55:28 +0100

    Added firebase

--- src/components/messageBox.jsx
+++ src/components/messageBox.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import trim from 'trim';
+import Firebase from 'firebase';
 
 class MessageBox extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            message: ''
+        }
+        
+        this.firebaseRef = new Firebase('https://plrl-react-stack.firebaseio.com/messages');        
+    }
+    
+    onChange(evnt) {
+        this.setState({
+           message: evnt.target.value 
+        });
+    }
+    
+    onKeyUp(evnt) {
+        if (evnt.keyCode === 13 && trim(evnt.target.value)) {
+            evnt.preventDefault();
+            this.setState({
+                message: ''
+            });
+            
+            console.log('sent msg', evnt.target.value);
+            this.firebaseRef.push({
+                message: this.state.message
+            });
+        }
     }
     
     render() {
@@ -11,16 +38,21 @@ class MessageBox extends React.Component {
                 maxWidth: 1200,
                 margin: '30px auto',
                 padding: 30
-            }}><textarea style={{
-                width: '100%',
-                borderColor: '#D0D0D0',
-                resize: 'none',
-                borderRadius: 3,
-                minHeigth: 50,
-                height: 75,
-                color: '#555',
-                fontSize: 14,
-                outline: 'auto 0px'
+            }}>
+            <textarea 
+                value={this.state.message}
+                onChange={this.onChange.bind(this)}
+                onKeyUp={this.onKeyUp.bind(this)}
+                style={{
+                    width: '100%',
+                    borderColor: '#D0D0D0',
+                    resize: 'none',
+                    borderRadius: 3,
+                    minHeigth: 50,
+                    height: 75,
+                    color: '#555',
+                    fontSize: 14,
+                    outline: 'auto 0px'
             }}/></div>
         )
     }
--- src/components/messageList.jsx
+++ src/components/messageList.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import Message from './message.jsx';
+import Firebase from 'firebase';
+import _ from 'lodash';
 
 class MessageList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            messages: [
-                'hi HI',
-                'there THERE THERE!!! OMG asdas  ',
-                'testing'
-            ]
-        }
+            messages: {}
+        };
+        
+        this.firebaseRef = new Firebase('https://plrl-react-stack.firebaseio.com/messages');
+        this.firebaseRef.on('child_added', msg => {
+           if (this.state.messages[msg.key()]) {
+               return;
+           }
+           
+           let msgVal = msg.val();
+           msgVal.key = msg.key();
+           this.state.messages[msgVal.key] = msgVal;
+           this.setState({messages: this.state.messages});
+        });
     } 
     
     render() {
-        var messageNodes = this.state.messages.map((message) => {
+        var messageNodes = _.values(this.state.messages).map((message) => {
            return (
-             <Message key={message} message={message} />  
+             <Message key={message.key} message={message.message} />  
            );
         });
         
